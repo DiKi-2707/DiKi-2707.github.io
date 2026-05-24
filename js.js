2469980f3744bceda99cb3694fb79a477a6f7a99
Eduard <89991876+DiKi-2707@users.noreply.github.com>
2026-05-24 16:08:52 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,24 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
 
+const glow = document.querySelector(".cursor-glow");
 
-const messages = [
-  "Ну вот, уже первый баг — пользователь не читает инструкции.",
-  "Ты уверен, что всё работает правильно?)",
-  "Баги есть всегда. Вопрос — где.",
-  "Тест пройден. Но это ничего не значит.",
-  "Ты уверен, что всё работает правильно?",
-  "Кажется, ты что-то упустил",
-
-];
-button.addEventListener('click', function () {
-  const randomIndex = Math.floor(Math.random() * messages.length);
-  alert(messages[randomIndex]);
+document.addEventListener("mousemove", (e) => {
+  glow.style.left = `${e.clientX}px`;
+  glow.style.top = `${e.clientY}px`;
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
